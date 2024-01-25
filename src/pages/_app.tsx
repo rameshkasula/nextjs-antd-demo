e17ef151a98fde41f9bdcc764d31b170ca3a14db
rameshkasula <rameshlearningadvance@gmail.com>
2024-01-25 18:16:23 +0530

    updated

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import "@/styles/globals.css";
+import React from "react";
+// import { ConfigProvider } from "antd";
 import type { AppProps } from "next/app";
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    // <ConfigProvider theme={AppTheme}>
+    <Component {...pageProps} />
+    // </ConfigProvider>
+  );
+};
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+export default App;
---
+++ src/theme/themeConfig.ts
@@ -0,0 +1,11 @@
+// theme/themeConfig.ts
+import type { ThemeConfig } from "antd";
+
+const AppTheme: ThemeConfig = {
+  token: {
+    fontSize: 16,
+    colorPrimary: "#52c41a",
+  },
+};
+
+export default AppTheme;
